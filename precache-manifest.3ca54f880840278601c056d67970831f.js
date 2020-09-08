e881fdfd823cdad3b254db390ababf0260e6ebcd
HAMADA.AYmen <aymen.hamada@epitech.eu>
2020-09-08 19:31:39 +0200

    Updates

---
+++ precache-manifest.3ca54f880840278601c056d67970831f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee675c72e238c679718a6722e1f4e29",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "97f6c9f170363076b02a",
+    "url": "/Portfolio/static/css/main.a8552cf3.chunk.css"
+  },
+  {
+    "revision": "689cf452ef6e218756c6",
+    "url": "/Portfolio/static/js/2.fc0e6883.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.fc0e6883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f6c9f170363076b02a",
+    "url": "/Portfolio/static/js/main.7a3b2a4e.chunk.js"
+  },
+  {
+    "revision": "796235efddf27470bbfe",
+    "url": "/Portfolio/static/js/runtime-main.25f07560.js"
+  },
+  {
+    "revision": "8ec80d51fe287a5a083be53920b46e8d",
+    "url": "/Portfolio/static/media/bitmoji.8ec80d51.png"
+  },
+  {
+    "revision": "d91b289906243b5d13b6d5d0c8148f0f",
+    "url": "/Portfolio/static/media/cinema.d91b2899.png"
+  },
+  {
+    "revision": "e74740f0220659620113cd4f14dd954c",
+    "url": "/Portfolio/static/media/dang.e74740f0.png"
+  },
+  {
+    "revision": "1474d023858929febf221d105cd60eeb",
+    "url": "/Portfolio/static/media/freeads.1474d023.png"
+  },
+  {
+    "revision": "b3e65b94b37b0368d605319d4aa23713",
+    "url": "/Portfolio/static/media/meetic.b3e65b94.png"
+  },
+  {
+    "revision": "218eab63326eaf79df5ed84b1afa97fc",
+    "url": "/Portfolio/static/media/mvc.218eab63.png"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/Portfolio/static/media/node.521444a2.png"
+  },
+  {
+    "revision": "f72311e3c9c79bf5b530758864b0baa9",
+    "url": "/Portfolio/static/media/notube.f72311e3.png"
+  },
+  {
+    "revision": "868bbf6a2af0daf06e271d19545556d0",
+    "url": "/Portfolio/static/media/sao.868bbf6a.png"
+  },
+  {
+    "revision": "e4c29576ae7acb8213ee9a77227091a1",
+    "url": "/Portfolio/static/media/spotify.e4c29576.png"
+  },
+  {
+    "revision": "ab6a9451df73440af1f737f9b58571a7",
+    "url": "/Portfolio/static/media/twitter.ab6a9451.png"
+  },
+  {
+    "revision": "f1ddd4762fcaebb4f79d2086abcb9bac",
+    "url": "/Portfolio/static/media/ying.f1ddd476.png"
+  }
+]);
